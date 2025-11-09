9e54353f9c7828fc3bededbcb324a2bef32d7faf
abhishek94443 <abhishekdwivedi9444@gmail.com>
2025-11-10 04:17:11 +0530

    Auth,Tenant,all security configs completed.

--- src/main/java/com/myapp/kinesis/KinesisApplication.java
+++ src/main/java/com/myapp/kinesis/KinesisApplication.java
@@ -2,12 +2,16 @@ package com.myapp.kinesis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enables automatic setting of createdAt/updatedAt fields
+@EnableScheduling // Enables @Scheduled for cron jobs (like our "No-Show" checker)
 public class KinesisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KinesisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KinesisApplication.class, args);
+    }
 
 }
